b24deee39ea1f62403775b6e96bbd3fbb441d611
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-30 12:30:01 +0100

    Add string methods

---
+++ MoreDataTypesApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MoreDataTypesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int typeOfSpecification = 1;
+            var myString = " C# list fundamentals ";
+            Console.WriteLine(StringExercise(myString, typeOfSpecification));
+        }
+
+        public static string StringExercise(String myString, int typeOfSpecification)
+        {
+            //string result = string.Empty;
+            //switch (typeOfSpecification)
+            //{
+            //    case 1:
+            //        result = myString.Trim();
+            //        break;
+            //    case 2:
+            //        result = myString.ToUpper();
+            //        break;
+            //    case 3:
+            //        result = myString.Replace('L', '*').Replace('N', '*');
+            //        break;
+            //    case 4:
+            //        int index = myString.IndexOf("N");
+            //        if (index >= 0)
+            //            result = result.Substring(0, index);
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+
+            var trimmedString = myString.Trim();
+            var ucString = trimmedString.ToUpper();
+            var replacedString = ucString.Replace('L', '*');
+            var replacedString2 = replacedString.Replace('T', '*');
+            var nPos = replacedString2.IndexOf('N');
+            var finalString = replacedString2.Remove(nPos + 1);
+            return finalString;
+
+            //return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MoreDataTypesApp
+
+### This is important 
+
+![Any PascalCase supports out there?: ProgrammerHumor](https://preview.redd.it/h6ndidikr5d31.png?auto=webp&s=701f3402bfc32169d66c3f2b19dd011b67a6462a)
+
+> I need to remember the name of the notation. 
